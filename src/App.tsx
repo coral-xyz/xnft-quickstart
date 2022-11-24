a0efc6747ddb088981c1528b3bff67308f99a629
Philipp Adrian <greenish@users.noreply.github.com>
2022-11-24 14:15:29 -0300

    Update quickstart to use react-native. (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import ReactXnft, { Text, View } from "react-xnft";
+import React from "react";
+import { Button, View } from "react-native";
+import { atom, RecoilRoot, useRecoilState } from "recoil";
+import tw from "twrnc";
 
-//
-// On connection to the host environment, warm the cache.
-//
-ReactXnft.events.on("connect", () => {
-  // no-op
+const testAtom = atom<"native" | "bright">({
+  key: "testAtom",
+  default: "native",
 });
 
-export function App() {
+function Root() {
+  const [future, setFuture] = useRecoilState(testAtom);
   return (
-    <View>
-      <Text>Hello, World!</Text>
+    <View
+      style={tw`h-full ${
+        future === "bright" ? "bg-yellow-100" : "bg-blue-100"
+      } flex justify-center items-center`}
+    >
+      <Button
+        title={`The Future is ${future}`}
+        color={future === "bright" ? "rgb(228, 208, 10)" : "rgb(33, 150, 243)"}
+        onPress={() => setFuture(future === "bright" ? "native" : "bright")}
+      />
     </View>
+  );
+}
+export default function App() {
+  return (
+    <RecoilRoot>
+      <Root />
+    </RecoilRoot>
   );
 }
