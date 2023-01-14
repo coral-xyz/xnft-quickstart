945fdaf085c6fd4f90e94b1248b615581587d5a5
wentokay <101902546+wentokay@users.noreply.github.com>
2023-01-14 16:00:08 +0000

    use react-refresh in development (#17)

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 const fs = require("fs");
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // keep everything the same for expo start
-  if(env.mode === "development") {
+  if (env.mode === "development") {
+    config.plugins.push(new ReactRefreshWebpackPlugin());
+    // keep everything else the same for expo start
     return config;
   }
 
   config.output = {
-    globalObject: 'this',
+    globalObject: "this",
     path: __dirname + "/dist/.artifacts/",
-    filename: 'index.js',
+    filename: "index.js",
   };
 
   config.optimization.splitChunks = {
@@ -22,28 +25,32 @@ module.exports = async function (env, argv) {
   };
   config.optimization.runtimeChunk = false;
 
-
-  config.plugins = config.plugins.filter(
-    (plugin) => ["DefinePlugin", "CleanWebpackPlugin"].includes(plugin.constructor.name)
-  )
+  config.plugins = config.plugins.filter((plugin) =>
+    ["DefinePlugin", "CleanWebpackPlugin"].includes(plugin.constructor.name)
+  );
 
   config.plugins.push(
     new InlineJSPlugin({
       template: "template.html",
-      filename: "index.html"
+      filename: "index.html",
     })
   );
 
-
   // this is brittle but works for now.
-  const loaders = config.module.rules.find(rule => typeof rule.oneOf !== "undefined");
-  const urlLoader = loaders.oneOf.find((loader) => typeof loader.use === "object" && loader.use.loader && loader.use.loader.includes("url-loader"));
+  const loaders = config.module.rules.find(
+    (rule) => typeof rule.oneOf !== "undefined"
+  );
+  const urlLoader = loaders.oneOf.find(
+    (loader) =>
+      typeof loader.use === "object" &&
+      loader.use.loader &&
+      loader.use.loader.includes("url-loader")
+  );
 
   urlLoader.use.options.limit = true;
   urlLoader.test = /\.(gif|jpe?g|png|svg|css|woff2?|eot|ttf|otf)$/;
 
   return config;
-
 };
 
 // const logger = console.log.bind(console);
@@ -52,17 +59,23 @@ class InlineJSPlugin {
   constructor({ template, filename }) {
     this.options = {
       template,
-      filename
-    }
+      filename,
+    };
   }
   apply(compiler) {
-    compiler.hooks.done.tap('InlineJSPlugin', (stats) => {
+    compiler.hooks.done.tap("InlineJSPlugin", (stats) => {
       const filename = stats.compilation.outputOptions.filename;
       const path = stats.compilation.outputOptions.path;
       const asset = stats.compilation.assets[filename];
       const JSBundle = asset.children[0]._value;
-      const template = fs.readFileSync(this.options.template).toString().split("####JS####");
-      fs.writeFileSync(path + "/../" + this.options.filename, template[0] + JSBundle + template[1]);
+      const template = fs
+        .readFileSync(this.options.template)
+        .toString()
+        .split("####JS####");
+      fs.writeFileSync(
+        path + "/../" + this.options.filename,
+        template[0] + JSBundle + template[1]
+      );
     });
   }
 }
