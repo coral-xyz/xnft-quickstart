ba3d7eb8bda76bb0c488afb097cf732dd92cbdea
Thales <81639334+cinojosa0705@users.noreply.github.com>
2023-01-16 12:55:36 -0600

    Adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,12 @@
 import { registerRootComponent } from "expo";
 import { RecoilRoot } from "recoil";
-import { ActivityIndicator, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { ExamplesScreens } from "./screens/ExamplesScreen";
 import { HomeScreen } from "./screens/HomeScreen";
-import { TokenListNavigator } from "./screens/TokenNavigator";
-
-const Tab = createBottomTabNavigator();
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: "#e91e63",
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="About"
-        component={TokenListNavigator}
-        options={{
-          headerShown: false,
-          tabBarLabel: "About",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bank" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Trade"
-        component={ExamplesScreens}
-        options={{
-          tabBarLabel: "Trade",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
 
 function App() {
-
-
-
   return (
     <RecoilRoot>
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
+        <HomeScreen/>
     </RecoilRoot>
   );
 }
